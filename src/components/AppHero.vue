4b26e05ef4f3f3928e63fc6bccff9f1ae339a473
Federico-Maiocchi <maiocchifederico@gmail.com>
2023-11-03 15:55:26 +0100

    ADD: new components

---
+++ src/components/AppHero.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  data() {
+    return {
+      addComponent: 'nuovo componente'
+    }
+  },
+  methods: {
+    
+  }
+}
+</script>
+
+<template>
+  <p>{{ addComponent }}</p> 
+  
+</template>
+
+<style scoped>
+.messaggio {
+  color: blue;
+}
+</style>
